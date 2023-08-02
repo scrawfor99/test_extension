be2f9f8fd9e1ba35c37cd796e5d15d8da9aad936
Stephen Crawford <steecraw@amazon.com>
2023-08-02 15:23:11 -0400

    Swap to gradle
    Signed-off-by: Stephen Crawford <steecraw@amazon.com>

--- src/main/java/crud/CrudAction.java
+++ src/main/java/crud/CrudAction.java
@@ -22,7 +22,7 @@ import org.opensearch.rest.NamedRoute;
 import org.opensearch.rest.RestRequest;
 import org.opensearch.rest.RestRequest.Method;
 import org.opensearch.rest.RestResponse;
-import org.opensearch.rest.RestStatus;
+import org.opensearch.core.rest.RestStatus;
 import org.opensearch.sdk.ExtensionsRunner;
 import org.opensearch.sdk.rest.BaseExtensionRestHandler;
 
